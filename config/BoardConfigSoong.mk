612dbbb283ae32b184dbe661b5084dbf0e365ec9
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-09-07 22:47:14 +0530

    vendor: soong: Add TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB
    Recently LOS changed KeyHandler to service. Service doesn't use system_server context, so FileObserver of old implementation can't access client info file. This implementation directly writes package name without using any files.
    
    Signed-off-by: Shantanu Sarkar <shantanuplussarkar@gmail.com>
    Change-Id: Ie69bac2adb8150f6954730907a9ec447877647cb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_dotGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_needs_client_info \
+    camera_needs_client_info_lib \
     has_legacy_camera_hal1 \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
@@ -68,6 +69,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_dotGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_dotGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_dotGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_dotGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_dotGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
